31cbd1ca55f08dcb1fce8b067903ee7a1d95a920
Jonnadula Chaitanya <sparkscj234@gmail.com>
2024-07-24 07:20:43 +0530

    added: dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HeroesComponent } from './heroes/heroes.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HeroesComponent } from "./heroes/heroes.component";
+import { QuoteDetailsComponent } from "./quote-details/quote-details.component";
 
 const routes: Routes = [
-  { path: 'heroes', component: HeroesComponent }
+  { path: "", redirectTo: "/heroes", pathMatch: "full" },
+  { path: "heroes", component: HeroesComponent },
+  {path:"quotes",component:QuoteDetailsComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { QuoteService } from '../services/quote.service';
 import { Quote } from '../quote';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-quote-details',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor,NgIf],
   templateUrl: './quote-details.component.html',
   styleUrl: './quote-details.component.css'
 })
